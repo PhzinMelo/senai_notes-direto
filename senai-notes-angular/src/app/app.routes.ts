decdf6da722899acade4ee93fda2bd58474d127f
Pedro Melo <pedrohenriquemelo1010@gmail.com>
2025-09-24 21:30:23 -0300

    configuracoes concluidas

--- senai-notes-angular/src/app/app.routes.ts
+++ senai-notes-angular/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { TelaLoginNotes } from './user-module/tela-login-notes/tela-login-notes';
+import { NewUserNotes } from './user-module/new-user-notes/new-user-notes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        loadComponent: () => TelaLoginNotes,
+        pathMatch: 'full' // ← aqui estava faltando a vírgula
+    },
+    {
+        path: 'new',
+        loadComponent: () => NewUserNotes
+    }
+];
